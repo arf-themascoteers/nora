ecc0f92b6ddde6da5e7d5d277ac841ffaab644dd
Arif <arif@themascoteers.com>
2023-07-27 17:20:03 +1000

    done

--- clipper.py
+++ clipper.py
@@ -1,32 +1,23 @@
 import rasterio
-from pyproj import Transformer
 from get_bounding_box import get_bounding_box
 from rasterio.windows import Window
 from rasterio.warp import transform_bounds
 from rasterio.crs import CRS
 
 
-jp2_file_path = 'test.jp2'
-out_file = "out.tif"
-
-min_x, min_y, max_x, max_y = get_bounding_box()
-
-with rasterio.open(jp2_file_path) as src:
-    epsg_4326 = CRS.from_epsg(4326)
-    min_x, min_y, max_x, max_y = transform_bounds(epsg_4326, src.crs, min_x, max_y, max_x, min_y)
-    window = src.window(min_x, min_y, max_x, max_y)
-    data = src.read(window=window)
-
-    profile = src.profile
-    profile.update({
-        'height': window.height,
-        'width': window.width,
-        'transform': rasterio.windows.transform(window, src.transform),
-        'nodata': None  # Optionally set the nodata value for the clipped raster
-    })
-
-    # Write the clipped raster to the output file
-    with rasterio.open(out_file, 'w', **profile) as dst:
-        dst.write(data)
-
-print("Raster clipping completed.")
+def clip(source,dest):
+    min_x, min_y, max_x, max_y = get_bounding_box()
+    with rasterio.open(source) as src:
+        epsg_4326 = CRS.from_epsg(4326)
+        min_x, min_y, max_x, max_y = transform_bounds(epsg_4326, src.crs, min_x, max_y, max_x, min_y)
+        window = src.window(min_x, min_y, max_x, max_y)
+        data = src.read(window=window)
+        profile = src.profile
+        profile.update({
+            'height': window.height,
+            'width': window.width,
+            'transform': rasterio.windows.transform(window, src.transform),
+            'nodata': None
+        })
+        with rasterio.open(dest, 'w', **profile) as dst:
+            dst.write(data)
--- extract_s2.py
+++ extract_s2.py
@@ -1,8 +1,52 @@
 import os
+import pandas as pd
+import numpy as np
+from clipper import clip
+import shutil
 
+TEST = True
+source_csv = "vectis.csv"
+if TEST:
+    source_csv = "vectis_min.csv"
 
-csv_path = os.path.join("data","final.csv")
+source_csv_path = os.path.join("data",source_csv)
+dest_csv_path = os.path.join("data","final.csv")
+dest_band_folder_path = os.path.join("data", "bands")
+
+if os.path.exists(dest_csv_path):
+    shutil.rmtree(dest_band_folder_path)
+
+os.mkdir(dest_band_folder_path)
+
 base = r"D:\Data\Sentinel-2\S2B_MSIL2A_20220423T002659_N0400_R016_T54HXE_20220423T021724\S2B_MSIL2A_20220423T002659_N0400_R016_T54HXE_20220423T021724.SAFE\GRANULE\L2A_T54HXE_A026783_20220423T003625\IMG_DATA"
+
+source_df = pd.read_csv(source_csv_path)
+num_rows = source_df.shape[0]
+data = np.zeros((num_rows,22))
+
+done = []
+
+
+def clip_band(source_band_path, dest_band_path):
+    clip(source_band_path, dest_band_path)
+
+
+def process():
+    for resolution in os.listdir(base):
+        resolution_path = os.path.join(base, resolution)
+        for file_name in os.listdir(resolution_path):
+            if file_name.endswith(".jp2"):
+                parts = file_name.split("_")
+                band = parts[2]
+                if band not in done:
+                    done.append(band)
+                    source_band_path = os.path.join(resolution_path, file_name)
+                    dest_band_path = os.path.join(dest_band_folder_path, band)
+                    clip_band(source_band_path, dest_band_path, source_csv_path)
+
+
+if __name__ == "__main__":
+    process()
 
 
 
