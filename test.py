fa3f782c33095ac3f43ef9704ee0f5cac3dbe2ca
Arif <arif@themascoteers.com>
2023-08-10 21:05:40 +1000

    test

--- test.py
+++ test.py
@@ -2,16 +2,12 @@ from evaluator import Evaluator
 
 
 if __name__ == "__main__":
-    # configs = ["vis","props","vis-props","bands","upper-vis", "upper-vis-props","all_ex_som"]
-    # configs = ["vis"]
-    # configs = [
-    #     {
-    #         "input":["B02","B03"],
-    #         "ag": "high",
-    #         "scenes": ["S2A_MSIL2A_20220207T002711_N0400_R016_T54HWE_20220207T023040"],
-    #         "name" : "shamsu"
-    #     }
-    # ]
-    configs = ["vis","props_ex_som","vis_props_ex_som","bands","all_ex_som"]
-    c = Evaluator(configs=configs, algorithms=["mlr","ann"],prefix="low",folds=3)
+    configs = [
+        {
+            "input":"props_ex_som",
+            "ag": "low",
+            "scenes": ["S2B_MSIL2A_20220413T002709_N0400_R016_T54HXE_20220413T021511"]
+        }
+    ]
+    c = Evaluator(configs=configs, algorithms=["mlr","ann"],prefix="test",folds=2)
     c.process()
