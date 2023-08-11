af138ce0b139c97de2f140aabdfed571316178b3
Arif <arif@themascoteers.com>
2023-08-11 12:09:23 +1000

    test

--- test_fold_ev.py
+++ test_fold_ev.py
@@ -7,7 +7,12 @@ if __name__ == "__main__":
             "input":"all_ex_som",
             "ag": "low",
             "scenes": ["S2B_MSIL2A_20220413T002709_N0400_R016_T54HXE_20220413T021511"]
+        },
+        {
+            "input": "vis",
+            "ag": "low",
+            "scenes": ["S2B_MSIL2A_20220413T002709_N0400_R016_T54HXE_20220413T021511"]
         }
     ]
-    c = FoldEvaluator(configs=configs, algorithms=["mlr","ann"], prefix="test2", folds=2)
+    c = FoldEvaluator(configs=configs, algorithms=["mlr","ann"], prefix="test_fold", folds=4)
     c.process()
