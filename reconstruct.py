1e8f8767fc076db9d146bc9a698d5703bfc4a62f
Arif <arif@themascoteers.com>
2023-08-11 10:34:46 +1000

    test

--- reconstruct.py
+++ reconstruct.py
@@ -3,23 +3,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def recon(csv):
+def recon(csv, height=None, width=None):
     df = None
     if isinstance(csv, str):
-        df = pd.read_csv(f)
+        df = pd.read_csv(csv)
     else:
         df = csv
     df = df[df["scene"] == 1]
-    min_row = df["row"].min()
-    max_row = df["row"].max()
-    min_col = df["column"].min()
-    max_col = df["column"].max()
-    print(min_row, max_row, min_col, max_col)
-    min_row = int(min_row)
-    max_row = int(max_row)
-    min_col = int(min_col)
-    max_col = int(max_col)
-    x = np.zeros((max_row+1,max_col+1),dtype=np.float64)
+    if height is None or width is None:
+        max_row = df["row"].max()
+        max_col = df["column"].max()
+        height = int(max_row)
+        width = int(max_col)
+    x = np.zeros((height+1,width+1),dtype=np.float64)
     for i in range(len(df)):
         row = df.loc[i,"row"]
         col = df.loc[i,"column"]
@@ -29,8 +25,13 @@ def recon(csv):
         x[row,col] = pix
     plt.imshow(x)
     plt.show()
+    return height, width
 
 
 if __name__ == "__main__":
-    f = r"data/processed/47eb237b21511beb392f4845d460e399/ag.csv"
-    recon(f)
+    f1 = r"data/processed/47eb237b21511beb392f4845d460e399/ag.csv"
+    f2 = r"data/processed/47eb237b21511beb392f4845d460e399/train_spatial.csv"
+    f3 = r"data/processed/47eb237b21511beb392f4845d460e399/test_spatial.csv"
+    height, width = recon(f1)
+    recon(f2, height, width)
+    recon(f3, height, width)
--- splitter.py
+++ splitter.py
@@ -14,10 +14,17 @@ class Splitter:
             return model_selection.train_test_split(df, test_size=0.2, random_state=2)
         df.sort_values(["row","column","scene"], inplace=True)
         total = len(df)
+        test_portion = 0.2
         test_count = int(total*.2)
-        train_count = total - test_count
-        train = df[0:train_count]
-        test = df[train_count:]
+
+        train_portion = 1 - test_portion
+        train_portion_each_block = train_portion/2
+        train_count_first_block = int(total * train_portion_each_block)
+
+        train_first = df[0:train_count_first_block]
+        test = df[train_count_first_block:train_count_first_block+test_count]
+        train_second = df[train_count_first_block+test_count:]
+        train = pd.concat([train_first, train_second])
         return train, test
 
 
