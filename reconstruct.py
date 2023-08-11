902789fef860ae6772a7975792c29ab24f65c520
Arif <arif@themascoteers.com>
2023-08-11 12:08:10 +1000

    test

--- reconstruct.py
+++ reconstruct.py
@@ -1,37 +1,57 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from csv_collector import CSVCollector
+from splitter import Splitter
 
 
-def recon(csv, height=None, width=None):
-    df = None
-    if isinstance(csv, str):
-        df = pd.read_csv(csv)
-    else:
-        df = csv
-    df = df[df["scene"] == 1]
-    if height is None or width is None:
-        max_row = df["row"].max()
-        max_col = df["column"].max()
-        height = int(max_row)
-        width = int(max_col)
-    x = np.zeros((height+1,width+1),dtype=np.float64)
-    for i in range(len(df)):
-        row = df.loc[i,"row"]
-        col = df.loc[i,"column"]
-        pix = df.loc[i,"B03"]
-        row = int(row)
-        col = int(col)
-        x[row,col] = pix
-    plt.imshow(x)
-    plt.show()
-    return height, width
+class Reconstructor:
+    @staticmethod
+    def recon(csv, height=None, width=None):
+        df = None
+        if isinstance(csv, str):
+            df = pd.read_csv(csv)
+        else:
+            df = csv
+        df = df[df["scene"] == 1]
+        if height is None or width is None:
+            max_row = df["row"].max()
+            max_col = df["column"].max()
+            height = int(max_row)
+            width = int(max_col)
+        x = np.zeros((height+1,width+1),dtype=np.float64)
+        for i in df.index:
+            row = df.loc[i,"row"]
+            col = df.loc[i,"column"]
+            pix = df.loc[i,"B03"]
+            row = int(row)
+            col = int(col)
+            x[row,col] = pix
+        plt.imshow(x)
+        plt.show()
+        return height, width
+
+    @staticmethod
+    def recon_folder(folder):
+        paths = CSVCollector.collect(folder)
+        height, width = Reconstructor.recon(paths["ag_csv_path"])
+        Reconstructor.recon(paths["train_spatial_csv_path"], height, width)
+        Reconstructor.recon(paths["test_spatial_csv_path"], height, width)
 
 
 if __name__ == "__main__":
+    basedir = r"data/processed/47eb237b21511beb392f4845d460e399"
     f1 = r"data/processed/47eb237b21511beb392f4845d460e399/ag.csv"
-    f2 = r"data/processed/47eb237b21511beb392f4845d460e399/train_spatial.csv"
-    f3 = r"data/processed/47eb237b21511beb392f4845d460e399/test_spatial.csv"
-    height, width = recon(f1)
-    recon(f2, height, width)
-    recon(f3, height, width)
+    # f2 = r"data/processed/47eb237b21511beb392f4845d460e399/train_spatial.csv"
+    # f3 = r"data/processed/47eb237b21511beb392f4845d460e399/test_spatial.csv"
+    # height, width = Reconstructor.recon(f1)
+    # Reconstructor.recon(f2, height, width)
+    # Reconstructor.recon(f3, height, width)
+
+    height, width = Reconstructor.recon(f1)
+
+    spl = Splitter(f1, mode="spatial", strat="bottom")
+    train, test = spl.split()
+
+    Reconstructor.recon(train, height, width)
+    Reconstructor.recon(test, height, width)
--- splitter.py
+++ splitter.py
@@ -3,10 +3,12 @@ import pandas as pd
 
 
 class Splitter:
-    def __init__(self, csv, mode="random"):
+    def __init__(self, csv, mode="random",strat="mid"):
         #mode = random, spatial
+        #strat = top, bottom, right, left, mid
         self.csv = csv
         self.mode = mode
+        self.strat = strat
 
     def split(self):
         df = pd.read_csv(self.csv)
@@ -16,22 +18,28 @@ class Splitter:
         total = len(df)
         test_portion = 0.2
         test_count = int(total*.2)
-
         train_portion = 1 - test_portion
-        train_portion_each_block = train_portion/2
-        train_count_first_block = int(total * train_portion_each_block)
+        train_count = total - test_count
+        train, test = None, None
 
-        train_first = df[0:train_count_first_block]
-        test = df[train_count_first_block:train_count_first_block+test_count]
-        train_second = df[train_count_first_block+test_count:]
-        train = pd.concat([train_first, train_second])
+        if self.strat == "mid":
+            train_portion_each_block = train_portion/2
+            train_count_first_block = int(total * train_portion_each_block)
+
+            train_first = df[0:train_count_first_block]
+            test = df[train_count_first_block:train_count_first_block+test_count]
+            train_second = df[train_count_first_block+test_count:]
+            train = pd.concat([train_first, train_second])
+
+        elif self.strat == "top":
+            train = df[0: train_count]
+            test = df[train_count:]
+
+        elif self.strat == "bottom":
+            test = df[0:test_count]
+            train = df[test_count: ]
+
         return train, test
 
 
-if __name__ == "__main__":
-    import reconstruct
-    f = r"data/processed/47eb237b21511beb392f4845d460e399/ag.csv"
-    s = Splitter(f, mode="spatial")
-    train, test = s.split()
-    reconstruct.recon(train)
-    reconstruct.recon(test)
+
