b0e786bd7169e08bff6a25e0dbc5d94f7ec5da5f
Arif <arif@themascoteers.com>
2023-08-21 09:38:58 +1000

    done

--- test_single_fold_ev.py
+++ test_single_fold_ev.py
@@ -3,7 +3,6 @@ from single_fold_evaluator import SingleFoldEvaluator
 
 if __name__ == "__main__":
     base_config = {
-        "ag": "low",
         "scene": "S2B_MSIL2A_20220413T002709_N0400_R016_T54HXE_20220413T021511"
     }
     inputs = ["vis", "props_ex_som", "vis_props_ex_som", "bands", "all_ex_som"]
