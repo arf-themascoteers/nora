8ec209a0dfbf1f0fe5c096cc8c72559f253ba07b
Arif <arif@themascoteers.com>
2023-08-21 01:38:10 +1000

    done

--- split_evaluator.py
+++ split_evaluator.py
@@ -34,7 +34,7 @@ class SplitEvaluator:
             scenes_count.append(len(scenes))
             scenes_string.append(scenes)
 
-        self.reporter = SplitReporter(prefix, self.config_list, scenes_count, scenes_string, algorithms)
+        self.reporter = SplitReporter(prefix, self.config_list, scenes_count, scenes_string, self.algorithms)
 
     def process(self):
         for index_algorithm, algorithm in enumerate(self.algorithms):
