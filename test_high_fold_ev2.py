1f6b565ffa3a46f635540f4153608c40ff24a375
Arif <arif@themascoteers.com>
2023-08-21 05:06:27 +1000

    done

--- test_high_fold_ev2.py
+++ test_high_fold_ev2.py
@@ -5,7 +5,13 @@ if __name__ == "__main__":
     base_config = {
     }
     inputs = [
+        ["B04","B03","B02"],
+        ["B04","B03","B02","B08"],
+        ["B08"],
         ["elevation", "moisture", "temp"],
+        ["elevation", "moisture", "temp","B04","B03","B02"],
+        ["elevation", "moisture", "temp","B04","B03","B02","B08"],
+        ["elevation", "moisture", "temp","B08"]
     ]
     configs = []
 
@@ -14,5 +20,5 @@ if __name__ == "__main__":
         a_config["input"] = i
         configs.append(a_config)
 
-    c = HighFoldEvaluator(configs=configs, prefix="high_fold_2", folds=10, algorithms=["mlr"])
+    c = HighFoldEvaluator(configs=configs, prefix="high_fold", folds=2, algorithms=["ann"])
     c.process()
