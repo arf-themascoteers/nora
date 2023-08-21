a042dd01e3e0b983c495a60e0a8711a82c8bd9b1
Arif <arif@themascoteers.com>
2023-08-21 10:21:43 +1000

    done

---
+++ test_combo_individual.py
@@ -0,0 +1,18 @@
+from combo_fold_evaluator import ComboFoldEvaluator
+
+
+if __name__ == "__main__":
+    base_config = {
+        "ag": "low",
+        "scenes": ["S2B_MSIL2A_20220413T002709_N0400_R016_T54HXE_20220413T021511"]
+    }
+    inputs = [[i] for i in ["B01", "B02", "B03", "B04", "B05", "B06", "B07", "B08", "B8A", "B09", "B11", "B12", "red", "green", "blue", "elevation", "moisture", "temp"]]
+    configs = []
+
+    for i in inputs:
+        a_config = base_config.copy()
+        a_config["input"] = i
+        configs.append(a_config)
+
+    c = ComboFoldEvaluator(configs=configs, prefix="fold", folds=10)
+    c.process()
